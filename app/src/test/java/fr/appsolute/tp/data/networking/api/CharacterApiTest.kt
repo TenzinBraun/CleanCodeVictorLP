a290f3c52a40c5a99d470b79941c300948c703d5
TenzinBraun <tenzin.braun@gmail.com>
2019-12-10 15:56:55 +0100

    CharacterApiTest correction

--- app/src/test/java/fr/appsolute/tp/data/networking/api/CharacterApiTest.kt
+++ app/src/test/java/fr/appsolute/tp/data/networking/api/CharacterApiTest.kt
@@ -16,7 +16,58 @@ class CharacterApiTest {
 
     private lateinit var instance: HttpClientManager
     private lateinit var api: CharacterApi
-    private lateinit var rick:Character
+    private val rick:Character = Character(
+        1,
+        "Rick Sanchez",
+        "Alive",
+        "Human",
+        "",
+        "Male",
+        Character.Place(
+            "Earth (C-137)",
+            "https://rickandmortyapi.com/api/location/1"
+        ),
+        Character.Place(
+            "Earth (Replacement Dimension)",
+            "https://rickandmortyapi.com/api/location/20"
+        ),
+        "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        listOf(
+            "https://rickandmortyapi.com/api/episode/1",
+            "https://rickandmortyapi.com/api/episode/2",
+            "https://rickandmortyapi.com/api/episode/3",
+            "https://rickandmortyapi.com/api/episode/4",
+            "https://rickandmortyapi.com/api/episode/5",
+            "https://rickandmortyapi.com/api/episode/6",
+            "https://rickandmortyapi.com/api/episode/7",
+            "https://rickandmortyapi.com/api/episode/8",
+            "https://rickandmortyapi.com/api/episode/9",
+            "https://rickandmortyapi.com/api/episode/10",
+            "https://rickandmortyapi.com/api/episode/11",
+            "https://rickandmortyapi.com/api/episode/12",
+            "https://rickandmortyapi.com/api/episode/13",
+            "https://rickandmortyapi.com/api/episode/14",
+            "https://rickandmortyapi.com/api/episode/15",
+            "https://rickandmortyapi.com/api/episode/16",
+            "https://rickandmortyapi.com/api/episode/17",
+            "https://rickandmortyapi.com/api/episode/18",
+            "https://rickandmortyapi.com/api/episode/19",
+            "https://rickandmortyapi.com/api/episode/20",
+            "https://rickandmortyapi.com/api/episode/21",
+            "https://rickandmortyapi.com/api/episode/22",
+            "https://rickandmortyapi.com/api/episode/23",
+            "https://rickandmortyapi.com/api/episode/24",
+            "https://rickandmortyapi.com/api/episode/25",
+            "https://rickandmortyapi.com/api/episode/26",
+            "https://rickandmortyapi.com/api/episode/27",
+            "https://rickandmortyapi.com/api/episode/28",
+            "https://rickandmortyapi.com/api/episode/29",
+            "https://rickandmortyapi.com/api/episode/30",
+            "https://rickandmortyapi.com/api/episode/31"
+        ),
+        "https://rickandmortyapi.com/api/character/1",
+        "2017-11-04T18:48:46.250Z"
+    )
 
 
     @Before
@@ -102,8 +153,6 @@ class CharacterApiTest {
             )
             println("${this.body()?.results?.count()}")
         }
-
-        rick = firstCharacter
 
         return@runBlocking
     }
